b6062699f4033632c0161710ffc03a24dcea4bf8
Jonathan Barney <jb@jonathanbarney.com>
2019-01-23 02:11:25 -0700

    fixing auth

--- tritium/urls.py
+++ tritium/urls.py
@@ -67,4 +67,5 @@ router.register(r'accounts', views.AccountViewSet)
 urlpatterns += [
     path('', include(router.urls),name='home'),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
