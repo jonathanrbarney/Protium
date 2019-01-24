d517c0bd83de09519d52ae821d86c5926a5be950
Jonathan Barney <jb@jonathanbarney.com>
2019-01-24 13:48:40 -0700

    Renaming

--- protium/wsgi.py
+++ protium/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tritium project.
+WSGI config for protium project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tritium.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'protium.settings')
 
 application = get_wsgi_application()
